1171bb2da1e371261e88223ec9454ec7ff422a6f
andrianam <andriana.mkrtchyan@gmail.com>
2025-09-24 14:07:26 +0000

    Adding source files

---
+++ assignments/homework-4/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "math_utils.h"
+
+int main(){
+	
+	int a = 4;
+	int sq = square(a);
+	printf("Number: %d, Square: %d\n", a, sq);
+	return 0;
+}
---
+++ assignments/homework-4/math_utils.c
@@ -0,0 +1,6 @@
+#include "math_utils.h"
+
+// implementing the function
+int square(int a){
+	return a * a; 
+}
